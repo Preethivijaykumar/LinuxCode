7a3f281c6419e2ed88c02bbb6613427f610ea39e
Preethi B V <41036441+Preethivijaykumar@users.noreply.github.com>
2021-08-16 12:14:51 +0530

    Update NpciSimulatorApplicationTests.java

--- NPCISimulator/src/test/java/com/npcisimulator/NpciSimulatorApplicationTests.java
+++ NPCISimulator/src/test/java/com/npcisimulator/NpciSimulatorApplicationTests.java
@@ -3,11 +3,7 @@ package com.npcisimulator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class NpciSimulatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+class NpciSimulatorApplicationTests {
 
 }
